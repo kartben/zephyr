8801673ba5351f7be2f93027210bdd71a7e58705
Benjamin Cabé <benjamin@zephyrproject.org>
2023-10-09 18:56:46 +0200

    sample posix app for 3.5 demo
    Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

--- samples/posix/gettimeofday/src/main.c
+++ samples/posix/gettimeofday/src/main.c
@@ -1,35 +1,75 @@
-/*
- * Copyright (c) 2019 Linaro Limited
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include <stdio.h>
+#include <pthread.h>
 #include <stdlib.h>
-#include <errno.h>
-#include <sys/time.h>
 #include <time.h>
-#include <unistd.h>
 
-int main(void)
-{
-	struct timeval tv;
+pthread_mutex_t guess_mutex;
+pthread_mutexattr_t guess_mutex_attr;
+pthread_cond_t guess_cond;
+pthread_condattr_t guess_cond_attr;
 
-	while (1) {
-		int res = gettimeofday(&tv, NULL);
-		time_t now = time(NULL);
-		struct tm tm;
-		localtime_r(&now, &tm);
+int correct_guess = 0;
 
-		if (res < 0) {
-			printf("Error in gettimeofday(): %d\n", errno);
-			return 1;
-		}
+void *guessing_game(void *arg) {
+    int target = *(int *)arg;
+    int guess;
 
-		printf("gettimeofday(): HI(tv_sec)=%d, LO(tv_sec)=%d, "
-		       "tv_usec=%d\n\t%s\n", (unsigned int)(tv.tv_sec >> 32),
-		       (unsigned int)tv.tv_sec, (unsigned int)tv.tv_usec,
-		       asctime(&tm));
-		sleep(1);
-	}
+    pthread_mutex_lock(&guess_mutex);
+    while (!correct_guess) {
+        printf("Enter your guess: ");
+        scanf("%d", &guess);
+
+        if (guess > target) {
+            printf("Too high!\n");
+        } else if (guess < target) {
+            printf("Too low!\n");
+        } else {
+            printf("Congratulations! You've guessed the number.\n");
+            correct_guess = 1;
+            pthread_cond_signal(&guess_cond);
+        }
+    }
+    pthread_mutex_unlock(&guess_mutex);
+    pthread_exit(NULL);
+}
+
+int main() {
+    pthread_t game_thread;
+
+    srand(time(NULL));
+    int random_number = rand() % 100 + 1;  // A random number between 1 and 100
+
+    pthread_mutexattr_init(&guess_mutex_attr);
+    pthread_mutex_init(&guess_mutex, &guess_mutex_attr);
+
+    pthread_condattr_init(&guess_cond_attr);
+    pthread_cond_init(&guess_cond, &guess_cond_attr);
+
+    // Create a new thread to run the guessing game
+    if (pthread_create(&game_thread, NULL, guessing_game, &random_number)) {
+        printf("Error creating thread.\n");
+        return 1;
+    }
+
+    pthread_mutex_lock(&guess_mutex);
+    while (!correct_guess) {
+        pthread_cond_wait(&guess_cond, &guess_mutex);
+    }
+    pthread_mutex_unlock(&guess_mutex);
+
+    // Cleanup
+    pthread_mutex_destroy(&guess_mutex);
+    pthread_mutexattr_destroy(&guess_mutex_attr);
+    pthread_cond_destroy(&guess_cond);
+    pthread_condattr_destroy(&guess_cond_attr);
+
+    // Wait for the game to finish
+    if (pthread_join(game_thread, NULL)) {
+        printf("Error joining thread.\n");
+        return 2;
+    }
+
+    printf("Bye!\n");
+
+    return 0;
 }
