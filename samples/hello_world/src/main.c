e625dd34e848740f46c62338cdace62253a1864c
Benjamin Cabé <benjamin@zephyrproject.org>
2023-10-12 11:40:15 +0200

    demo35: basic hello world sample
    Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -5,9 +5,19 @@
  */
 
 #include <stdio.h>
+#include <math.h>
 
+/**
+ * west build -p auto -b stm32f401_mini -t rom_report samples/hello_world -- -DCONFIG_MINIMAL_LIBC=y
+ * west build -p auto -b stm32f401_mini -t rom_report samples/hello_world -- -DCONFIG_NEWLIB_LIBC=y
+ * west build -p auto -b stm32f401_mini -t rom_report samples/hello_world -- -DCONFIG_NEWLIB_LIBC_NANO=y
+ * west build -p auto -b stm32f401_mini -t rom_report samples/hello_world -- -DCONFIG_PICOLIBC=y
+ *
+ */
 int main(void)
 {
 	printf("Hello World! %s\n", CONFIG_BOARD);
+	printf("sqrt(2) = %f\n", sqrt(2.0));
+	printf("sin(π) = %f\n", sin(3.141592));
 	return 0;
 }
