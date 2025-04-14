kartben/zephyr

commit 38b87905ba2c72cd41ecad615974eac6009a7a1f

doc: _scripts: boards: fix condition for revision
Revision might be `0` so update the condition to not inadvertently
exclude boards with such revision number.

Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

doc/_scripts/gen_boards_catalog.py
@@ -138,7 +138,7 @@ def gather_board_devicetrees(twister_out_dir):
                 revision = board_info.get('revision', '')
 
                 board_target = board_name
-                if revision:
+                if revision is not None:
                     board_target = f"{board_target}@{revision}"
                 if qualifier:
                     board_target = f"{board_target}/{qualifier}"

commit 49dbf9b00e9694642798a7fd7d35fb38d7f2b2fb

doc: _scripts: boards: fix catalog for boards without revisions
Commit 38b8790 introduced a pretty bad regression for boards without
revisions. Fix by correctly checking for undefined revisions (and
qualifiers while at it).

Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

doc/_scripts/gen_boards_catalog.py
@@ -159,13 +159,13 @@ def gather_board_build_info(twister_out_dir):
                 build_info = yaml.safe_load(f)
                 board_info = build_info.get('cmake', {}).get('board', {})
                 board_name = board_info.get('name')
-                qualifier = board_info.get('qualifiers', '')
-                revision = board_info.get('revision', '')
+                qualifier = board_info.get('qualifiers')
+                revision = board_info.get('revision')
 
                 board_target = board_name
                 if revision is not None:
                     board_target = f"{board_target}@{revision}"
-                if qualifier:
+                if qualifier is not None:
                     board_target = f"{board_target}/{qualifier}"
 
                 with open(edt_pickle_file, 'rb') as f: