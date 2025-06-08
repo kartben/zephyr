2c7da9cefc9f6d45d80435f360bb819faa71443b
Benjamin Cabé <kartben@gmail.com>
2025-06-08 13:29:06 +0200

    tests: sys_util: expand macro coverage

--- tests/lib/sys_util/src/main.c
+++ tests/lib/sys_util/src/main.c
@@ -7,6 +7,7 @@
 #include <zephyr/ztest.h>
 #include <zephyr/sys/util.h>
 #include <zephyr/kernel.h>
+#include <errno.h>
 
 /**
  * @addtogroup sys_util_tests
@@ -65,7 +66,57 @@ ZTEST(sys_util, test_NUM_VA_ARGS_LESS_1)
 	zassert_equal(0, NUM_VA_ARGS_LESS_1(_1));
 	zassert_equal(1, NUM_VA_ARGS_LESS_1(_1, _2));
 	/* support up to 64 args */
-	zassert_equal(63, NUM_VA_ARGS_LESS_1(LISTIFY(64, ~, (,))));
+        zassert_equal(63, NUM_VA_ARGS_LESS_1(LISTIFY(64, ~, (,))));
+}
+
+ZTEST(sys_util, test_clamp_inrange)
+{
+       zassert_equal(CLAMP(5, 0, 10), 5);
+       zassert_equal(CLAMP(-1, 0, 10), 0);
+       zassert_equal(CLAMP(20, 0, 10), 10);
+
+       zassert_true(IN_RANGE(5, 0, 10));
+       zassert_false(IN_RANGE(-1, 0, 10));
+       zassert_false(IN_RANGE(11, 0, 10));
+}
+
+ZTEST(sys_util, test_log2ceil_nhpot)
+{
+       zassert_equal(LOG2CEIL(1), 0);
+       zassert_equal(LOG2CEIL(2), 1);
+       zassert_equal(LOG2CEIL(3), 2);
+       zassert_equal(LOG2CEIL(8), 3);
+
+       zassert_equal(NHPOT(1), 1U);
+       zassert_equal(NHPOT(5), 8U);
+       zassert_equal(NHPOT(0), 1U);
+       zassert_equal(NHPOT((1ULL << 63) + 1), 0ULL);
+}
+
+ZTEST(sys_util, test_hex_conversions)
+{
+       uint8_t val;
+       char c;
+
+       zassert_ok(char2hex('a', &val));
+       zassert_equal(val, 10);
+       zassert_ok(hex2char(10, &c));
+       zassert_equal(c, 'a');
+
+       zassert_equal(char2hex('z', &val), -EINVAL);
+       zassert_equal(hex2char(20, &c), -EINVAL);
+
+       const uint8_t in[] = {0xde, 0xad, 0xbe, 0xef};
+       char hex[9];
+       uint8_t out[4];
+       size_t len;
+
+       len = bin2hex(in, sizeof(in), hex, sizeof(hex));
+       zassert_equal(len, 8);
+       zassert_mem_equal(hex, "deadbeef", len);
+
+       zassert_equal(hex2bin(hex, len, out, sizeof(out)), sizeof(out));
+       zassert_mem_equal(out, in, sizeof(in));
 }
 /**
  * @}
