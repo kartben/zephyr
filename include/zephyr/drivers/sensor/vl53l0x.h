dd39badc985bb70c3cdfbe4f8fcee4d6b3060ba3
Benjamin Cabé <benjamin@zephyrproject.org>
2025-08-13 17:29:18 +0200

    wip
    Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

--- include/zephyr/drivers/sensor/vl53l0x.h
+++ include/zephyr/drivers/sensor/vl53l0x.h
@@ -28,20 +28,57 @@
 
 /* VL53L0x specific channels */
 enum sensor_channel_vl53l0x {
+	/**
+	 * @brief Maximum range
+	 *
+	 * Maximum range of the sensor, in millimeters.
+	 */
 	SENSOR_CHAN_VL53L0X_RANGE_DMAX = SENSOR_CHAN_PRIV_START,
+
+	/**
+	 * @brief Signal rate
+	 *
+	 * Signal rate, in counts per second.
+	 */
 	SENSOR_CHAN_VL53L0X_SIGNAL_RATE_RTN_CPS,
+
+	/**
+	 * @brief Ambient rate
+	 *
+	 * Ambient rate, in counts per second.
+	 */
 	SENSOR_CHAN_VL53L0X_AMBIENT_RATE_RTN_CPS,
+
+	/**
+	 * @brief Effective SPAD count
+	 *
+	 * Effective SPAD (Single Photon Avalanche Diode) count.
+	 */
 	SENSOR_CHAN_VL53L0X_EFFECTIVE_SPAD_RTN_COUNT,
+
+	/**
+	 * @brief Range status
+	 *
+	 * Range status, available in the sensor_value.val1 field.
+	 * Possible values are defined as @a VL53L0X_RANGE_STATUS_* macros.
+	 */
 	SENSOR_CHAN_VL53L0X_RANGE_STATUS,
 };
 
-/* VL53L0x meas status values */
-#define VL53L0X_RANGE_STATUS_RANGE_VALID    (0)
-#define VL53L0X_RANGE_STATUS_SIGMA_FAIL	    (1)
-#define VL53L0X_RANGE_STATUS_SIGNAL_FAIL    (2)
-#define VL53L0X_RANGE_STATUS_MIN_RANGE_FAIL (3)
-#define VL53L0X_RANGE_STATUS_PHASE_FAIL	    (4)
-#define VL53L0X_RANGE_STATUS_HARDWARE_FAIL  (5)
-#define VL53L0X_RANGE_STATUS_NO_UPDATE      (255)
+/**
+ * @name Possible values for SENSOR_CHAN_VL53L0X_RANGE_STATUS channel
+ *
+ * @details See UM2039 User Manual, Rev. 2.0, Section 5.3.1 / Table 1.
+ * @{
+ */
+#define VL53L0X_RANGE_STATUS_RANGE_VALID    (0)		/**< Range valid */
+#define VL53L0X_RANGE_STATUS_SIGMA_FAIL	    (1)		/**< Sigma fail */
+#define VL53L0X_RANGE_STATUS_SIGNAL_FAIL    (2)		/**< Signal fail */
+#define VL53L0X_RANGE_STATUS_MIN_RANGE_FAIL (3)		/**< Min range fail */
+#define VL53L0X_RANGE_STATUS_PHASE_FAIL	    (4)		/**< Phase fail */
+#define VL53L0X_RANGE_STATUS_HARDWARE_FAIL  (5)		/**< Hardware fail */
+#define VL53L0X_RANGE_STATUS_NO_UPDATE      (255)	/**< No update */
+
+/** @} */
 
 #endif /* ZEPHYR_INCLUDE_DRIVERS_SENSOR_VL53L0X_H_ */
