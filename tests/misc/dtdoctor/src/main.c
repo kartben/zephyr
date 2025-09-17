3f6805f16f9ce3f5636cea394865dd5653fb3fb4
Benjamin Cabé <benjamin@zephyrproject.org>
2025-09-17 23:33:29 +0200

    an attempt at testing dtdoctor
    Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

---
+++ tests/misc/dtdoctor/src/main.c
@@ -0,0 +1,30 @@
+/*
+ * SPDX-FileCopyrightText: Copyright The Zephyr Project Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/gpio.h>
+
+#define LED0_NODE DT_ALIAS(led0)
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+int main(void)
+{
+	int ret;
+
+	if (!gpio_is_ready_dt(&led)) {
+		return 0;
+	}
+
+	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return 0;
+	}
+
+	gpio_pin_toggle_dt(&led);
+
+	return 0;
+}
